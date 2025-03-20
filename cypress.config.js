855c74cebfc67027812be0d350b0fc6c92b90e82
hhaider18 <hussain14.cs@gmail.com>
2025-03-20 13:43:02 +0500

    NewFile1

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,11 @@ module.exports = defineConfig({
   },
 });
 module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
   reporter: "mochawesome",
   reporterOptions: {
     reportDir: "cypress/reports",
